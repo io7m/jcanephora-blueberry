566bb2035b5e4cf9d146c781b366baddabbe1e30
Mark Raynsford <code@io7m.com>
2015-10-30 13:53:10 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+io7m-jcanephora-blueberry
+=========================
+
+https://io7m.github.io/jcanephora/
+https://io7m.github.io/blueberry/
+
---
+++ src/main/java/com/io7m/jcanephora/blueberry/Main.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2015 <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.jcanephora.blueberry;
+
+import com.io7m.blueberry.TestReportConfig;
+import com.io7m.blueberry.gui.GUI;
+import com.io7m.blueberry.gui.GUIProjectInfo;
+import com.io7m.blueberry.gui.GUIProjectVersion;
+import com.io7m.junreachable.UnreachableCodeException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import java.net.URI;
+
+/**
+ * The main frontend.
+ */
+
+public final class Main
+{
+  private Main()
+  {
+    throw new UnreachableCodeException();
+  }
+
+  /**
+   * The main entry point.
+   *
+   * @param args Command line arguments
+   */
+
+  public static void main(final String[] args)
+  {
+    final TestReportConfig xml_config = new TestReportConfig();
+    xml_config.setOutputEnvironment(true);
+    xml_config.setOutputSystemProperties(true);
+
+    final GUIProjectVersion version = new GUIProjectVersion(0, 40, 0, "SNAPSHOT");
+    final GUIProjectInfo info = new GUIProjectInfo("jcanephora", version);
+    info.addPackagePrefix("com.io7m.jcanephora");
+    info.setProjectURI(URI.create("http://io7m.github.io/jcanephora"));
+
+    SwingUtilities.invokeLater(
+      () -> {
+        final GUI g = new GUI(info, xml_config);
+        g.getMainWindow()
+          .setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      });
+  }
+}
